e49847068858fe9a6a5703abc24e4b2507603b47
Engreiarst <ryuu.the.demonslayer@gmail.com>
2025-09-16 11:18:48 -0600

    Trying to relocate from normal VS Code to WSL Ubuntu distro in order to fix the time to parse.

--- stats.py
+++ stats.py
@@ -3,7 +3,7 @@ def get_num_words(text):
     words = text.split()
     return len(words)
 
-def get_num_characters(text="Text"):
+def get_num_characters(text="books/frankenstein.txt"):
     lowered = text.lower()
     uniques = set()
     character_count = dict()
@@ -11,3 +11,7 @@ def get_num_characters(text="Text"):
         uniques.add(char)
         character_count[char] = lowered.count(char)
     return character_count
+
+def sort_characters(character_count):
+    sorted_characters = sorted(character_count.items(), key=lambda x: x[1], reverse=True)
+    return sorted_characters.isalpha()
